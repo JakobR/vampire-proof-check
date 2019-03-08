6c5e44d452622a01b4132b15284761548f9836cf
Jakob Rath <git@jakobrath.eu>
2019-03-08 12:05:08 +0100

    Ensure that Data.DependencyGraph.resolve runs in linear time

--- src/Data/DependencyGraph.hs
+++ src/Data/DependencyGraph.hs
@@ -5,6 +5,10 @@ module Data.DependencyGraph
   , resolve
   ) where
 
+-- base
+import Control.Monad.ST (runST, ST)
+import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
+
 -- containers
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -12,8 +16,7 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 
 -- mtl
-import Control.Monad.Except (ExceptT, runExceptT, withExceptT, throwError)
-import Control.Monad.Reader (Reader, runReader, asks, local)
+import Control.Monad.Except (lift, ExceptT, runExceptT, withExceptT, throwError)
 
 -- recursion-schemes
 import Data.Functor.Foldable (Fix(..))
@@ -23,34 +26,42 @@ import Data.Functor.Foldable (Fix(..))
 resolve
   :: forall f k. (Traversable f, Ord k)
   => Map k (f k)
+     -- NOTE: an advantage of taking a Map as input is that we don't
+     -- have to worry about duplicate keys in the input.
   -> Either (Error k) (Map k (Fix f))
-resolve input =
+resolve input = runST $ do
+  refs :: Map k (STRef s (ResolveState f k)) <-
+    sequence $ (newSTRef . Todo) <$> input
+
   let
-    resolved :: Map k (ExceptT (InternalError k) (Reader (Set k)) (Fix f))
-    resolved = Map.mapWithKey resolveOne input
+    resolveOne :: Set k -> k -> STRef s (ResolveState f k) -> ExceptT (InternalError k) (ST s) (Fix f)
+    resolveOne visited j ref = do
+      val <- lift $ readSTRef ref
+      case val of
+        Ok x' -> return x'
+        Err e -> throwError e
+        Todo x -> do
+          let lookupDep' = lookupDep (Set.insert j visited) j
+          x' <- Fix <$> withExceptT (errorAt j) (traverse lookupDep' x)
+          lift $ writeSTRef ref (Ok x')
+          return x'
 
-    resolveOne :: k -> f k -> ExceptT (InternalError k) (Reader (Set k)) (Fix f)
-    resolveOne j = fmap Fix
-                   . withExceptT (errorAt j)
-                   . local (Set.insert j)
-                   . traverse (lookupDep j)
+    lookupDep :: Set k -> k -> k -> ExceptT (InternalError k) (ST s) (Fix f)
+    lookupDep visited j i
+      | i `Set.member` visited =
+          throwError (ICycleIncomplete i [])
+      | otherwise =
+          case refs Map.!? i of
+            Nothing -> throwError (IMissing i j)
+            Just ref -> resolveOne visited i ref
 
-    lookupDep :: k -> k -> ExceptT (InternalError k) (Reader (Set k)) (Fix f)
-    lookupDep j i =
-      case resolved Map.!? i of
-        Nothing ->
-          throwError (IMissing i j)
-        Just getDep -> do
-          isLooping <- asks (i `Set.member`)
-          if isLooping
-            then throwError (ICycleIncomplete i [])
-            else getDep
-
-    resolved' :: ExceptT (Error k) (Reader (Set k)) (Map k (Fix f))
-    resolved' = withExceptT fromInternal (sequence resolved)
-  in
-    runReader (runExceptT resolved') Set.empty
+  runExceptT . withExceptT fromInternal $ Map.traverseWithKey (resolveOne Set.empty) refs
 {-# INLINABLE resolve #-}
+
+data ResolveState f k
+  = Ok !(Fix f)
+  | Err !(InternalError k)
+  | Todo !(f k)
 
 data Error k
   = Missing !k !k
@@ -64,11 +75,9 @@ errorAt :: Eq k => k -> InternalError k -> InternalError k
 errorAt j (ICycleIncomplete i js)
   | i == j    = ICycle (j:js)
   | otherwise = ICycleIncomplete i (j:js)
--- errorAt j (IMissingIncomplete i) = IMissing i j
 errorAt _ e = e
 
 data InternalError k
-  -- = IMissingIncomplete !k
   = IMissing !k !k
   | ICycleIncomplete !k ![k]
   | ICycle ![k]
@@ -77,6 +86,5 @@ data InternalError k
 fromInternal :: InternalError k -> Error k
 fromInternal (IMissing i j) = Missing i j
 fromInternal (ICycle js) = Cycle js
--- fromInternal (IMissingIncomplete _) =  error "Data.DependencyGraph.IMissingIncomplete: should not appear"
 fromInternal (ICycleIncomplete i js) = error "Data.DependencyGraph.ICycleIncomplete: should not appear" $
                                        Cycle (i:js)
--- test/Data/DependencyGraphSpec.hs
+++ test/Data/DependencyGraphSpec.hs
@@ -1,15 +1,34 @@
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.DependencyGraphSpec
   ( spec
   ) where
 
+-- base
+import Control.Concurrent (threadDelay)
+import Control.Exception (evaluate)
+import Data.Either (isRight)
+import GHC.Generics (Generic1, Generic)
+
+-- async
+import Control.Concurrent.Async (race)
+
 -- containers
 import qualified Data.Map.Strict as Map
+
+-- deepseq
+import Control.DeepSeq (NFData, NFData1, force)
 
 -- deriving-compat
 import Data.Eq.Deriving (deriveEq1)
@@ -26,7 +45,15 @@ import Data.DependencyGraph
 
 
 data ThingF a = Thing String [a]
-  deriving (Eq, Show, Functor, Foldable, Traversable)
+  deriving (Eq, Show, Functor, Foldable, Generic, Generic1, Traversable)
+  deriving anyclass NFData
+  deriving anyclass NFData1
+
+-- -- Orphan instances for tests
+-- deriving stock instance Generic (Error k)
+-- deriving anyclass instance NFData k => NFData (Error k)
+-- deriving stock instance Generic (Fix f)
+-- deriving anyclass instance (forall x. NFData x => NFData (f x)) => NFData (Fix f)
 
 $(deriveEq1 ''ThingF)
 $(deriveShow1 ''ThingF)
@@ -34,6 +61,28 @@ $(deriveShow1 ''ThingF)
 
 resolveList :: [(Int, ThingF Int)] -> Either (Error Int) [(Int, Fix ThingF)]
 resolveList = fmap Map.toList . resolve . Map.fromList
+
+
+-- TODO: should use something like Benchmarkable in package criterion to ensure the computation
+-- isn't optimized away.
+-- Maybe we should just use criterion to run performance tests like this.
+shouldCompleteWithin :: HasCallStack => IO a -> Int -> Expectation
+shouldCompleteWithin action timeout = do
+  result <- race action (threadDelay timeout)
+  case result of
+    Left _ -> return ()
+    Right () -> expectationFailure ("did not complete within " <> show timeout <> "µs")
+
+
+shouldEvaluateWithin :: (HasCallStack, NFData a) => a -> Int -> Expectation
+shouldEvaluateWithin x timeout = do
+  evaluate (force x) `shouldCompleteWithin` timeout
+
+
+-- | The thing with id 1 has Θ(bf^n) dependencies.
+exponentialDependencies :: Int -> Int -> [(Int, ThingF Int)]
+exponentialDependencies bf n =
+  [ (i, Thing ("t" <> show i) (if i < n then replicate bf (i+1) else [])) | i <- [1 .. n] ]
 
 
 spec :: Spec
@@ -81,3 +130,15 @@ spec =
                   ]
         `shouldBe`
         Left (Cycle [2,3,4])
+
+    it "runs in linear time" $ do
+      -- We don't 'deepseq' the actual result, since it seems to do complete traversal of the data structure,
+      -- which will take Θ(2^1000) for this example (but memory usage of the fully evaluated value would
+      -- still be linear due to sharing).
+      -- Hence we just call isRight on the result. This does effectively the same work, since all
+      -- dependencies have to be resolved before deciding whether to return an error or not.
+      -- Since the value is valid, we don't return early due to an error.
+      isRight (resolveList (exponentialDependencies 2 1000))
+        `shouldEvaluateWithin` 100_000
+      isRight (resolveList (exponentialDependencies 2 1000))
+        `shouldBe` True
